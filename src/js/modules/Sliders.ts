a6e8ea4ad33c43f97266608b492ac22713aef126
Poylar <doaleks8@gmail.com>
2023-05-24 20:00:34 +0800

    Deploy

--- src/js/modules/Sliders.ts
+++ src/js/modules/Sliders.ts
@@ -1,6 +1,6 @@
-import Swiper, { Mousewheel, Navigation, Pagination } from 'swiper';
+import Swiper, { Mousewheel, Navigation, Pagination, EffectFade, Autoplay } from 'swiper';
 /* eslint-disable import/no-unresolved */
-import 'swiper/css';
+import 'swiper/css/bundle';
 
 // import breakpoints from '../utils/MatchMedia';
 
@@ -8,17 +8,26 @@ class Sliders {
   static cases() {
     const root = document.querySelector('.js-cases-slider') as HTMLElement;
     const sliderInstance = new Swiper(root, {
-      modules: [Mousewheel, Pagination],
+      modules: [Mousewheel, Pagination, Navigation],
       init: false,
-      loop: true,
-      slidesPerView: 2,
-      loopedSlides: 18,
-      centeredSlides: true,
+
+      slidesPerView: 1,
+
       spaceBetween: 30,
 
       mousewheel: {
         forceToAxis: true,
         sensitivity: 0.3,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+      },
+
+      navigation: {
+        prevEl: root.querySelector('.js-prev-slider') as HTMLElement,
+        nextEl: root.querySelector('.js-next-slider') as HTMLElement,
       },
     });
 
@@ -54,11 +63,40 @@ class Sliders {
 
     sliderInstance.init();
   }
+
+  static hero() {
+    const root = document.querySelector('.js-hero-slider') as HTMLElement;
+    const sliderInstance = new Swiper(root, {
+      modules: [Mousewheel, Navigation, EffectFade, Autoplay],
+      init: false,
+      
+      loop:true,
+      speed:500,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true,
+      },
+      autoplay: {
+        delay: 1500,
+      },
+      slidesPerView: 1,
+
+      spaceBetween: 30,
+
+      mousewheel: {
+        forceToAxis: true,
+        sensitivity: 0.3,
+      },
+    });
+
+    sliderInstance.init();
+  }
 }
 
 function slidersInit() {
   Sliders.cases();
   Sliders.reviews();
+  Sliders.hero();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
